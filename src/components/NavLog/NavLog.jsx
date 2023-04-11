e96fac3fa4a149f8ba135e2eddfb762741f9bd15
Diego Martinez <oozyboy.game@gmail.com>
2023-04-10 22:18:27 -0400

    Cambios en el navbar

--- src/components/NavLog/NavLog.jsx
+++ src/components/NavLog/NavLog.jsx
@@ -1,23 +1,66 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
+import { useState } from "react";
+import {
+	Navbar,
+	Typography,
+	IconButton,
+  } from "@material-tailwind/react";
+
+import imgUser from './user.svg';
+import fotoUSer from './user.png';
 
 const Main = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
 	};
+	const [estadoMenu, setEstadoMenu] = useState(false);
 	const token = localStorage.getItem("token");
 	return (
-		<div className="nav">
-			{token?
-			<div className="hom">
-				<nav className={styles.navbar}>
-					<h1>EventoUVM</h1>
-					<button className={styles.white_btn} onClick={handleLogout}>
-						Cerrar sesion
-					</button>
-				</nav>
-			</div>:
+		<div>
+			{ token 
+				? 
+				<div>
+					<Navbar className="mx-full max-w-screen-full py-2 px-4 lg:px-8 lg:py-4 bg-white flex text-titNav">
+					<div className="container mx-auto flex items-center justify-between">
+						<span className="text-2xl font-bold">EventoUVM</span>
+					</div>
+
+					<div className="flex container w-full h-full space-x-8 items-center justify-end">
+						<Link to={'/'} className="text-1xl font-bold cursor-pointer">
+							HOME
+						</Link>
+						<Link to={'/eventos'} className="text-1xl font-bold cursor-pointer">
+							EVENTOS
+						</Link>
+
+
+						<img src={imgUser} alt="Usuario" className="w-8 cursor-pointer" onClick={() =>setEstadoMenu(!estadoMenu)} />
+					</div>
+					</Navbar> 
+
+					{ estadoMenu 
+						? 
+						<div className="w-64 h-auto shadow-md rounded-lg absolute right-16 top-20 flex flex-col items-center justify-center bg-slate-50 p-8">
+							<div className="flex flex-col items-center pb-12">
+								<img src={fotoUSer} alt="User Foto" className="w-16" />
+								<span className="text-lg font-medium">Nombre del Usuario</span>
+							</div>
+							<div className="flex justify-between space-x-16">
+								<Link to={`/profile/${token}`} className="text-lg font-bold rounded-lg text-[#18551c] w-auto p-2 h-auto">
+									Perfil
+								</Link>
+								<button className="text-lg font-bold rounded-lg text-[#18551c] w-auto p-2 h-auto" onClick={handleLogout}>
+									Salir 
+								</button>
+							</div>
+	
+						</div> 
+						:
+						null
+					}
+				</div>:
 			<div className="navHo">
 				<nav className={styles.navbar}>
 					<h1>EventoUVM</h1>
@@ -35,7 +78,6 @@ const Main = () => {
 					</div>
 				</nav>
 			</div>}
-
 		</div>
 	);
 };
